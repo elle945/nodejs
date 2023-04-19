06418e07425078f6ad9111e30b75619656bb8a66
elle945 <106586120+elle945@users.noreply.github.com>
2023-04-19 15:26:31 +0200

    Redirection no express

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
-
 const http = require('http')
+let count = 0
 
-const app = http.createServer((req, res) => {
-    if ( req.url === '/') {
+const app = (req, res) => {
+    if (req.url === '/') {
         res.write('Hejsan svejsan')
-      } else if ( req.url === '/foo') {
+        res.end()
+    } else if (req.url === '/foo') {
         res.write('bar')
-      } else if (req.url === '/baz') {
+        res.end()
+    } else if (req.url === '/baz') {
         res.write('qux')
-      } else {
+        res.end()
+    } else if (req.url === '/count') {
+        if (req.method === 'GET') {
+            res.write(`${count}`)
+            res.end()
+        } else {
+            res.statusCode = 405
+            res.end()
+        }
+    } else if (req.url === '/increment')
+        if (req.method === 'POST') {
+            count++
+            res.end()
+        } else {
+            res.statusCode = 405
+            res.end()
+        }
+    else if (req.url.startsWith('/add/')) {
+        if (req.method === 'POST') {
+            const urlParam = parseInt(req.url.split('/').pop())
+            count += urlParam
+            res.end()
+        } else {
+            res.statusCode = 405
+            res.end()
+        }
+    } else {
         res.statusCode = 404
-      }
+        res.end()
+    }
+}
+const server = http.createServer((req, res) => {
+    if (req.url.startsWith('/redirect')) {
+        res.writeHead(301, { Location: 'http://localhost:3000/' })
+        res.end()
+    } else {
+        app(req, res)
+    }
+})
 
-      res.end()
-    })
-app.listen({ port: 8080 }, () => {
-    console.log('Server started')
+const redirection = http.createServer((req, res) => {
+    res.writeHead(302, { Location: 'http://localhost:3000/' })
+    res.end()
+})
+
+redirection.listen(8080, () => {
+    console.log('Server started on 8080')
+})
+
+server.listen(3000, () => {
+    console.log('Server started on 3000')
 })
