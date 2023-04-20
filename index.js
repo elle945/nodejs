0d10f20f72382d2ee46ab9093308a1158431dca4
elle945 <106586120+elle945@users.noreply.github.com>
2023-04-20 22:02:01 +0200

    query

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express')
-let useragent = require('express-useragent')
-
-userAgents = []
-values = []
 
 const app = express()
-app.use(useragent.express())
-app.get('/', (req, res) => {
-    res.send(req.useragent)
-})
-app.get('/log', (req, res) => {
-    const userAgent = req.useragent.source
-    const data = { userAgent: userAgent, time: new Date().toISOString() }
-    values.push(data)
-    res.send(values)
-})
-app.delete('/log', (req, res) => {
-    if (req.method === 'DELETE') {
-        values = []
-        res.send(values)
-    } else res.status(405)
+
+app.get('/add', (req, res) => {
+    const x = parseInt(req.query.x)
+    const y = parseInt(req.query.y)
+    if (isNaN(x) || isNaN(y)) {
+        res.status(400).send('Bad Request')
+    } else {
+        const sum = x + y
+        res.status(200).send(`${sum}`)
+    }
 })
 
+
 app.listen(8080, () => {
-    console.log('server on port 8080')
+    console.log('server 8080 started')
 })
 
 //HELP WITH GET REQUEST
